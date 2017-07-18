c20d7c11fa77a25983e1787560d321df65b6936f
VinsoyTisoy <vadingding@partnersolutions.com.ph>
2017-07-18 10:38:52 +0800

    Add files via upload

---
+++ instrument.bat
@@ -0,0 +1,10 @@
+set CLASSPATH=webcontentcc\WEB-INF\lib\activejdbc-1.4.13.j7.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\javalite-common-1.4.13.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\mysql-connector-java-5.1.42.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\slf4j-api-1.7.25.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\slf4j-simple-1.7.25.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\activejdbc-instrumentation-1.4.11.jar
+set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\javassist-3.8.0.GA.jar
+set CLASSPATH=%CLASSPATH%;bin
+
+java -classpath %CLASSPATH% -DoutputDirectory=bin org.javalite.instrumentation.Main
