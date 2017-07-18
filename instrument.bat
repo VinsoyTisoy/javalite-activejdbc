c4d9434fbcf5036aae9dc2dbfe35866623637dff
VinsoyTisoy <vadingding@partnersolutions.com.ph>
2017-07-18 10:40:10 +0800

    Update instrument.bat

--- instrument.bat
+++ instrument.bat
@@ -5,6 +5,6 @@ set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\slf4j-api-1.7.25.jar
 set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\slf4j-simple-1.7.25.jar
 set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\activejdbc-instrumentation-1.4.11.jar
 set CLASSPATH=%CLASSPATH%;webcontentcc\WEB-INF\lib\javassist-3.8.0.GA.jar
-set CLASSPATH=%CLASSPATH%;bin
+set CLASSPATH=%CLASSPATH%;webcontentbuild\classes
 
-java -classpath %CLASSPATH% -DoutputDirectory=bin org.javalite.instrumentation.Main
+java -classpath %CLASSPATH% -DoutputDirectory=webcontentbuild\classes org.javalite.instrumentation.Main
